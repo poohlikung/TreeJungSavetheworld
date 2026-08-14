d504b042f4df5a0d3d0936966f7fa85199b1ae9e
PoohSorawit <hillingstudio@gmail.com>
2026-08-15 05:22:50 +0700

    feat:first commit

---
+++ treejung/src/test/java/com/example/treejung/TreejungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.treejung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreejungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
